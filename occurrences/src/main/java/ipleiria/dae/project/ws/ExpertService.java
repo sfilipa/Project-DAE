525d04d1a62b74a0505aae215bbe73ba7c3f9dfe
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2023-01-04 23:05:42 +0000

    Expert Get Assigned Occurrences

--- occurrences/src/main/java/ipleiria/dae/project/ws/ExpertService.java
+++ occurrences/src/main/java/ipleiria/dae/project/ws/ExpertService.java
@@ -3,6 +3,8 @@ package ipleiria.dae.project.ws;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ipleiria.dae.project.dtos.ExpertDTO;
+import ipleiria.dae.project.dtos.InsuranceDTO;
+import ipleiria.dae.project.dtos.OccurrenceDTO;
 import ipleiria.dae.project.ejbs.ExpertBean;
 import ipleiria.dae.project.entities.Expert;
 import ipleiria.dae.project.exceptions.MyEntityExistsException;
@@ -25,6 +27,86 @@ import java.util.stream.Collectors;
 public class ExpertService {
     @EJB
     private ExpertBean expertBean;
+
+
+    /**
+     * Expert
+     *  Repairers
+     */
+
+    @PATCH
+    @Path("/{username}/occurrences/{occurrence_code}/acceptRepairer")
+    public Response acceptRepairer(@PathParam("username") String username, @PathParam("occurrence_code") long occurrence_code){
+        try{
+            expertBean.acceptRepairer(username, occurrence_code);
+            return Response.status(Response.Status.OK).build();
+        }catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @PATCH
+    @Path("/{username}/occurrences/{occurrence_code}/rejectRepairer")
+    public Response rejectRepairer(@Context HttpServletRequest request, @PathParam("username") String username, @PathParam("occurrence_code") long occurrence_code){
+        try{
+            // Get the input stream from the request
+            InputStream inputStream = request.getInputStream();
+
+            // Read the message body from the input stream
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(inputStream);
+
+            // Extract the values from the JSON object
+            String description = rootNode.get("description").asText();
+
+            expertBean.rejectRepairer(username, occurrence_code, description);
+            return Response.status(Response.Status.OK).build();
+        }catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+
+    /**
+     * Expert
+     *  Occurrences
+     */
+
+    @PATCH
+    @Path("/{username}/occurrences/{code}/assign")
+    public Response assignOccurrence(@PathParam("username") String username, @PathParam("code") String code) {
+        try{
+            expertBean.addOccurrence(username, code);
+            return Response.status(Response.Status.OK).build();
+        }catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @PATCH
+    @Path("/{username}/occurrences/{code}/unassign")
+    public Response unassignOccurrence(@PathParam("username") String username, @PathParam("code") String code) {
+        try{
+            expertBean.removeOccurrence(username, code);
+            return Response.status(Response.Status.OK).build();
+        }catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @GET
+    @Path("/{username}/occurrences/assigned")
+    public Response getAssignedOccurrences(@PathParam("username") String username) {
+        return Response.ok(OccurrenceDTO.from(expertBean.occurrences(username))).build();
+    }
 
     @PATCH
     @Path("/{username}/occurrences/{occurrence_code}/disapprove")
@@ -70,41 +152,10 @@ public class ExpertService {
         }
     }
 
-    @PATCH
-    @Path("/{username}/occurrences/{occurrence_code}/acceptRepairer")
-    public Response acceptRepairer(@PathParam("username") String username, @PathParam("occurrence_code") long occurrence_code){
-        try{
-            expertBean.acceptRepairer(username, occurrence_code);
-            return Response.status(Response.Status.OK).build();
-        }catch (Exception e) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(e.getMessage())
-                    .build();
-        }
-    }
-
-    @PATCH
-    @Path("/{username}/occurrences/{occurrence_code}/rejectRepairer")
-    public Response rejectRepairer(@Context HttpServletRequest request, @PathParam("username") String username, @PathParam("occurrence_code") long occurrence_code){
-        try{
-            // Get the input stream from the request
-            InputStream inputStream = request.getInputStream();
-
-            // Read the message body from the input stream
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode rootNode = mapper.readTree(inputStream);
-
-            // Extract the values from the JSON object
-            String description = rootNode.get("description").asText();
-
-            expertBean.rejectRepairer(username, occurrence_code, description);
-            return Response.status(Response.Status.OK).build();
-        }catch (Exception e) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(e.getMessage())
-                    .build();
-        }
-    }
+    /**
+     * Expert
+     *  CRUD
+     */
 
     @GET // means: to call this endpoint, we need to use the HTTP GET method
     @Path("/") // means: the relative url path is “/api/students/”
@@ -141,32 +192,6 @@ public class ExpertService {
         return Response.status(Response.Status.CREATED)
                 .entity(ExpertDTO.from(expert))
                 .build();
-    }
-
-    @PATCH
-    @Path("/{username}/occurrences/{code}/assign")
-    public Response assignOccurrence(@PathParam("username") String username, @PathParam("code") String code) {
-        try{
-            expertBean.addOccurrence(username, code);
-            return Response.status(Response.Status.OK).build();
-        }catch (Exception e) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(e.getMessage())
-                    .build();
-        }
-    }
-
-    @PATCH
-    @Path("/{username}/occurrences/{code}/unassign")
-    public Response unassignOccurrence(@PathParam("username") String username, @PathParam("code") String code) {
-        try{
-            expertBean.removeOccurrence(username, code);
-            return Response.status(Response.Status.OK).build();
-        }catch (Exception e) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(e.getMessage())
-                    .build();
-        }
     }
 
     @DELETE
